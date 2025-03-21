47fc6075bdd0eb69f3a422c745c3c10046a659f1
Ayush Kumar <159880913+ayush-086@users.noreply.github.com>
2025-03-21 23:13:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const express = require('express');
-const PORT = process.env.PORT || 5004;
-const {handler} = require('./controller');
+const { handleMessage } = require("./lib/Telegram");
 
+async function handler(req, method){
+    const {body} = req;
+    if(body){
+        const messageObj = body.message;
+        await(handleMessage(messageObj));
+    }
+    return;
 
-const app = express();
-app.use(express.json());
-app.post('*', async(req,res)=>{
-    console.log(req.body);
-    res.send(await handler(req));
-});
-app.get('*', async(req,res)=>{
-    res.send(await handler(req));
-});
+}
 
-app.listen(PORT, function(err){
-    if(err) console.log(err);
-    console.log(`Server is running on port ${PORT}`);
-});
-
-
+module.exports = {handler};
